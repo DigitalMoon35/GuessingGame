9d87126bbed1be4ee0b62a9f287934ee837c47ae
digitalmoon <freshlife35@yahoo.com>
2022-10-05 11:26:55 -0400

    created interface with readline

---
+++ guessing-game.js
@@ -0,0 +1,8 @@
+const { stdin, stdout } = require( "node:process" );
+const readline = require("node:readline");
+
+const rl = readline.createInterface({
+    input: stdin,
+    output: stdout,
+});
+
