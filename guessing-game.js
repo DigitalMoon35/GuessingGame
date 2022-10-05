0766770087c9656a6ac6c2a12f8012b96e384017
digitalmoon <freshlife35@yahoo.com>
2022-10-05 16:55:34 -0400

    added game loop, random number generator, and winning graphic

--- guessing-game.js
+++ guessing-game.js
@@ -1,40 +1,61 @@
 const readline = require("node:readline");
+const art = require("ascii-art");
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-const SECRET_NUMBER = 35;
+let SECRET_NUMBER;
 
+// choose random number between min & max
 function randomInRange(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-
+// Check user guess against secret number
 function checkGuess(guess) {
   if (guess > SECRET_NUMBER) {
-    console.log("Too High!")
+    console.log("Too High!");
     return false;
   } else if (guess < SECRET_NUMBER) {
-    console.log("Too Low!")
+    console.log("Too Low!");
     return false;
   } else {
-    console.log("Correct!");
+    console.log("You Guessed the Number!!");
     return true;
   }
 }
 
+// ask user to guess, ask again if incorrect. 
 function askGuess() {
-  rl.question("Guess a number \n", answer => {
+  rl.question("Guess a number: ", answer => {
     const correct = checkGuess(Number(answer));
     if (!correct) {
       askGuess()
     } else {
-      console.log("You won!");
+      art.font("You Won!", 'doom', (err, rendered) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(rendered);
+      });
       rl.close();
     }
   })
 }
 
-askGuess();
+// main function
+// initializes game with user input and prompts for guess.
+function askRange() {
+  rl.question("Enter a max number: ", max => {
+    rl.question("Enter a min number: ", min => {
+      console.log(`I'm thinking of a number between ${min} and ${max}...`)
+      SECRET_NUMBER = randomInRange(Number(min), Number(max));
+      askGuess();
+
+    });
+  });
+}
+
+askRange()
